5bb31297ccdf869a7fa69521fcaf045d1de472e5
GuiCelebrini <celebrinig@gmail.com>
2021-05-22 16:23:31 -0300

    Criando tela de validação

--- app/src/main/java/com/android/guicelebrini/whatsapp/activity/LoginActivity.java
+++ app/src/main/java/com/android/guicelebrini/whatsapp/activity/LoginActivity.java
@@ -2,15 +2,31 @@ package com.android.guicelebrini.whatsapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.android.guicelebrini.whatsapp.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private Button buttonCadastrar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        buttonCadastrar = findViewById(R.id.buttonCadastrar);
+
+        buttonCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent destino = new Intent(getApplicationContext(), ValidacaoActivity.class);
+                startActivity(destino);
+            }
+        });
+
     }
 }
