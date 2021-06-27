6a408045d22d1ad0cb54a05cd26b2f9a79ae263f
GuiCelebrini <celebrinig@gmail.com>
2021-06-27 17:05:22 -0300

    Creating token generator

--- app/src/main/java/com/android/guicelebrini/whatsapp/activity/LoginActivity.java
+++ app/src/main/java/com/android/guicelebrini/whatsapp/activity/LoginActivity.java
@@ -4,27 +4,53 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.android.guicelebrini.whatsapp.R;
 
-public class LoginActivity extends AppCompatActivity {
-    // ah é
-    private Button buttonCadastrar;
+import java.util.Random;
 
-    private EditText editTelefone;
+public class LoginActivity extends AppCompatActivity {
+    private Button buttonRegister;
+    private EditText editPersonName;
+    private EditText editNumberCountry ,editNumberDDD ,editNumberPhone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        buttonCadastrar = findViewById(R.id.buttonCadastrar);
-        editTelefone = findViewById(R.id.editTelefone);
+        findViewsById();
 
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = editPersonName.getText().toString();
+                String phoneNumber =
+                        editNumberCountry.getText().toString() +
+                                editNumberDDD.getText().toString() +
+                                editNumberPhone.getText().toString();
 
+                //gerar token
+                Random random = new Random();
+                int randomNumber = random.nextInt((9999-1000) + 1000);
+                String token = String.valueOf(randomNumber);
+
+                Log.i("Resultado", token);
+            }
+        });
+
+    }
+
+    public void findViewsById(){
+        buttonRegister = findViewById(R.id.buttonRegister);
+        editPersonName = findViewById(R.id.editPersonName);
+        editNumberCountry = findViewById(R.id.editNumberCountry);
+        editNumberDDD = findViewById(R.id.editNumberDDD);
+        editNumberPhone = findViewById(R.id.editNumberPhone);
     }
 
     public void trocarTela(View view){
