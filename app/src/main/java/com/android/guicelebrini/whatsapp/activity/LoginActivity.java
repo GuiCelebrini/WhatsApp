198ea455c97cdbc9501207ab8499725474d7061f
GuiCelebrini <celebrinig@gmail.com>
2021-07-05 20:08:59 -0300

    Creating User model

--- app/src/main/java/com/android/guicelebrini/whatsapp/activity/LoginActivity.java
+++ app/src/main/java/com/android/guicelebrini/whatsapp/activity/LoginActivity.java
@@ -25,7 +25,7 @@ public class LoginActivity extends AppCompatActivity {
 
         findViewsById();
         firebaseReference = FirebaseConfig.getFirebaseReference();
-        
+
 
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/android/guicelebrini/whatsapp/activity/RegisterActivity.java
+++ app/src/main/java/com/android/guicelebrini/whatsapp/activity/RegisterActivity.java
@@ -3,14 +3,44 @@ package com.android.guicelebrini.whatsapp.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.android.guicelebrini.whatsapp.R;
+import com.android.guicelebrini.whatsapp.model.User;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText editName, editEmail, editPassword;
+    private Button buttonRegister;
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        findViewsById();
+
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                user = new User(editName.getText().toString(), editEmail.getText().toString(), editPassword.getText().toString());
+                registerUser();
+
+            }
+        });
+    }
+
+    public void findViewsById(){
+        editName = findViewById(R.id.editRegisterName);
+        editEmail = findViewById(R.id.editRegisterEmail);
+        editPassword = findViewById(R.id.editRegisterPassword);
+        buttonRegister = findViewById(R.id.buttonRegister);
+    }
+
+    public void registerUser(){
+        
     }
 }
