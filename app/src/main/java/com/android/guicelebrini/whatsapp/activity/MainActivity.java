3c637b471e273ffd3a92b4fab920e9835a0bdf1d
GuiCelebrini <celebrinig@gmail.com>
2021-07-12 12:25:09 -0300

    Creating a toolbar and a menu on MainActivity

--- app/src/main/java/com/android/guicelebrini/whatsapp/activity/MainActivity.java
+++ app/src/main/java/com/android/guicelebrini/whatsapp/activity/MainActivity.java
@@ -1,23 +1,40 @@
 package com.android.guicelebrini.whatsapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import com.android.guicelebrini.whatsapp.R;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-    
-    private DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewsById();
+        setSupportActionBar(toolbar);
 
-        reference.child("pontos").setValue(500);
 
+    }
+
+    public void findViewsById(){
+        toolbar = findViewById(R.id.toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        return true;
     }
 }
