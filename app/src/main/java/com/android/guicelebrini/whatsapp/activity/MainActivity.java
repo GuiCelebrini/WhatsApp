1673407ec0e435d2623f0fb3733b48c5172955bb
GuiCelebrini <celebrinig@gmail.com>
2021-07-16 20:00:18 -0300

    Creating fragments and SlidingTabLayout

--- app/src/main/java/com/android/guicelebrini/whatsapp/activity/MainActivity.java
+++ app/src/main/java/com/android/guicelebrini/whatsapp/activity/MainActivity.java
@@ -3,6 +3,8 @@ package com.android.guicelebrini.whatsapp.activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,7 +13,9 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.android.guicelebrini.whatsapp.R;
+import com.android.guicelebrini.whatsapp.adapter.TabAdapter;
 import com.android.guicelebrini.whatsapp.config.FirebaseConfig;
+import com.android.guicelebrini.whatsapp.helper.SlidingTabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -19,6 +23,8 @@ import com.google.firebase.database.FirebaseDatabase;
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private SlidingTabLayout slidingTabLayout;
+    private ViewPager viewPager;
 
     private FirebaseAuth auth;
 
@@ -29,11 +35,24 @@ public class MainActivity extends AppCompatActivity {
         findViewsById();
         setSupportActionBar(toolbar);
 
+        //configuring adapter on viewPager
+        TabAdapter tabAdapter = new TabAdapter(getSupportFragmentManager(), 1);
+        viewPager.setAdapter(tabAdapter);
+
+        //configuring slidingTabLayout
+        slidingTabLayout.setDistributeEvenly(true);
+        slidingTabLayout.setSelectedIndicatorColors(ContextCompat.getColor(this, R.color.colorAccent));
+
+        //configuring viewPager on slidingTabLayout
+        slidingTabLayout.setViewPager(viewPager);
+
 
     }
 
     public void findViewsById(){
         toolbar = findViewById(R.id.toolbar);
+        slidingTabLayout = findViewById(R.id.stl_tabs);
+        viewPager = findViewById(R.id.vpMain);
     }
 
     public void logoutUser(){
@@ -43,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ app/src/main/java/com/android/guicelebrini/whatsapp/fragment/ChatsFragment.java
@@ -0,0 +1,31 @@
+package com.android.guicelebrini.whatsapp.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.guicelebrini.whatsapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ChatsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ChatsFragment extends Fragment {
+
+    public ChatsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chats, container, false);
+    }
+}
