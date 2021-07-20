a58dcd02f23245c4eca94881f1469fc3595dc736
GuiCelebrini <celebrinig@gmail.com>
2021-07-20 13:55:29 -0300

    Created a recyclerView on ContactsFragment

--- app/src/main/java/com/android/guicelebrini/whatsapp/fragment/ContactsFragment.java
+++ app/src/main/java/com/android/guicelebrini/whatsapp/fragment/ContactsFragment.java
@@ -3,12 +3,16 @@ package com.android.guicelebrini.whatsapp.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.android.guicelebrini.whatsapp.R;
+import com.android.guicelebrini.whatsapp.model.Contact;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +20,11 @@ import com.android.guicelebrini.whatsapp.R;
  * create an instance of this fragment.
  */
 public class ContactsFragment extends Fragment {
+
+    private RecyclerView recyclerContacts;
+    private List<Contact>  contactsList;
+    private View view;
+
 
     public ContactsFragment() {
         // Required empty public constructor
@@ -25,6 +34,14 @@ public class ContactsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contacts, container, false);
+        view = inflater.inflate(R.layout.fragment_contacts, container, false);
+
+        findViewsById();
+
+        return view;
+    }
+
+    public void findViewsById(){
+        recyclerContacts = view.findViewById(R.id.recyclerContacts);
     }
 }
