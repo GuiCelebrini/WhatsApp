18a66a80fb122956d96465f99b4a37d9f9b7bf28
GuiCelebrini <celebrinig@gmail.com>
2021-07-31 12:11:48 -0300

    Added onClick on RecyclerChats to access ChatActivity

--- app/src/main/java/com/android/guicelebrini/whatsapp/fragment/ChatsFragment.java
+++ app/src/main/java/com/android/guicelebrini/whatsapp/fragment/ChatsFragment.java
@@ -1,20 +1,33 @@
 package com.android.guicelebrini.whatsapp.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.LinearLayout;
 
 import com.android.guicelebrini.whatsapp.R;
+import com.android.guicelebrini.whatsapp.activity.ChatActivity;
 import com.android.guicelebrini.whatsapp.adapter.AdapterRecyclerChats;
+import com.android.guicelebrini.whatsapp.config.FirebaseConfig;
+import com.android.guicelebrini.whatsapp.helper.Base64Custom;
+import com.android.guicelebrini.whatsapp.helper.Preferences;
+import com.android.guicelebrini.whatsapp.helper.RecyclerItemClickListener;
 import com.android.guicelebrini.whatsapp.model.Chat;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,6 +39,9 @@ public class ChatsFragment extends Fragment {
     private AdapterRecyclerChats adapter;
 
     private View view;
+
+    private DatabaseReference reference;
+    private ValueEventListener eventListener;
 
     public ChatsFragment() {
         // Required empty public constructor
@@ -52,7 +68,31 @@ public class ChatsFragment extends Fragment {
     }
 
     private void createChatsList(){
-        
+
+        Preferences preferences = new Preferences(view.getContext());
+        String loggedUserId = preferences.getUserId();
+
+        reference = FirebaseConfig.getFirebaseReference();
+        reference = reference.child("chats").child(loggedUserId);
+
+        eventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                chatsList.clear();
+
+                for ( DataSnapshot data : snapshot.getChildren()) {
+                    Chat chat = data.getValue(Chat.class);
+                    chatsList.add(chat);
+                }
+
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+
+            }
+        };
     }
 
     private void configureRecyclerChats(){
@@ -62,6 +102,42 @@ public class ChatsFragment extends Fragment {
         recyclerChats.setLayoutManager(layoutManager);
         recyclerChats.setHasFixedSize(true);
         recyclerChats.addItemDecoration(new DividerItemDecoration(view.getContext(), LinearLayout.VERTICAL));
+
+        recyclerChats.addOnItemTouchListener(new RecyclerItemClickListener(getContext(), recyclerChats, new RecyclerItemClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onItemClick(View view, int position) {
+                String contactName = chatsList.get(position).getContactName();
+                String contactEmail = Base64Custom.decode(chatsList.get(position).getContactId());
+
+                Intent intent = new Intent(getActivity(), ChatActivity.class);
+                intent.putExtra("contactName", contactName);
+                intent.putExtra("contactEmail", contactEmail);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onLongItemClick(View view, int position) {
+
+            }
+        }));
+
         recyclerChats.setAdapter(adapter);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        reference.addValueEventListener(eventListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        reference.removeEventListener(eventListener);
     }
 }
--- app/src/main/java/com/android/guicelebrini/whatsapp/helper/Base64Custom.java
+++ app/src/main/java/com/android/guicelebrini/whatsapp/helper/Base64Custom.java
@@ -9,7 +9,7 @@ public class Base64Custom {
     }
 
     public static String decode(String encodedText){
-        return String.valueOf(Base64.encode(encodedText.getBytes(), Base64.DEFAULT));
+        return new String( Base64.decode(encodedText, Base64.DEFAULT) );
     }
 
 }
